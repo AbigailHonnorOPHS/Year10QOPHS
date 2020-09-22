153117e523fba7272ebdc285c580003dcc3e8880
Abi :) <71695665+AbigailHonnorOPHS@users.noreply.github.com>
2020-09-22 10:10:16 +0100

    Add files via upload

---
+++ 07 STRING SLICING 02.py
@@ -0,0 +1,8 @@
+firstname=str(input("what is your first name?>>>>> "))
+lastname=str(input("what is your last name?>>>>> "))
+
+wholename= firstname + lastname
+lengthofwholename= len(wholename)
+
+print(wholename)
+print(lengthofwholename)
---
+++ 07 STRING SLICING 03.py
@@ -0,0 +1,6 @@
+firstName=str(input("what is your first name?>>>>> ")).lower()
+lastName=str(input("what is your last name?>>>>> ")).lower()
+
+print(firstName)
+print("")
+print(lastName)
