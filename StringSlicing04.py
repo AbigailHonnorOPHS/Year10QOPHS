942647051dcc535df74c3144286d087274feca12
Abi :) <71695665+AbigailHonnorOPHS@users.noreply.github.com>
2021-02-11 11:12:38 +0000

    Delete StringSlicing04.py

--- StringSlicing04.py
+++
@@ -1,9 +0,0 @@
-NurseryRhyme= str(input("enter the first line of a nursery rhyme >>>>> "))
-LengthOfRhyme= len(NurseryRhyme)
-
-print("this has",LengthOfRhyme,"letters in it")
-
-NumberOne= int(input("please enter a number >>>>> "))
-NumberTwo= int(input("Please enter a second number >>>>> "))
-
-print(NurseryRhyme[NumberOne:NumberTwo])
