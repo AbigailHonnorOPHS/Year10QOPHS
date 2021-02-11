d225c6050909b0ed8999aeaea006f4d6b021972d
Abi :) <71695665+AbigailHonnorOPHS@users.noreply.github.com>
2021-02-11 11:12:17 +0000

    Delete StringSlicing02.py

--- StringSlicing02.py
+++
@@ -1,8 +0,0 @@
-firstname=str(input("what is your first name?>>>>> "))
-lastname=str(input("what is your last name?>>>>> "))
-
-wholename= firstname + lastname
-lengthofwholename= len(wholename)
-
-print(wholename)
-print(lengthofwholename)
